76415adb2c39aa7dd72c34adb0b40ea4583f9c51
Mike <34043825+Mlawrence95@users.noreply.github.com>
2018-12-03 05:24:11 -0800

    Add files via upload

--- infrastructure/User.py
+++ infrastructure/User.py
@@ -14,7 +14,7 @@ class User:
         return questionID in self.recorded.keys()
 
     def recordOutcome(questionID, outcome):
-            self.recorded[questionID] = outcome
+        self.recorded[questionID] = outcome
 
     def getAllHistory():
         return self.recorded

06cd21bf07537f4726b5c74715fa746066f1fda6
Mike <34043825+Mlawrence95@users.noreply.github.com>
2018-12-03 16:32:13 -0800

    Add files via upload

--- User.py
+++ User.py
@@ -12,15 +12,37 @@ class User:
     ## a name to display
 
     def hasSeen(self, questionID):
+        """
+        Returns true if questions has been answered already, 
+        else false
+        """
         return questionID in self.recorded.keys()
 
     def recordOutcome(self, questionID, outcome):
+        """
+        Given index of question and what the student answers
+
+        creates a record of the student's mark
+
+        returns nothing (void)
+        """
         self.recorded[questionID] = outcome
 
     def getAllHistory(self):
+        """
+        returns dictionary of raw performance results
+        """
         return self.recorded
 
     def getPerformance(self):
+        """
+        returns dataframe of:
+
+        question id (questionID)
+        correct answer (correct)
+        student response (response)
+        if student was correct  (outcome)
+        """
 
         qid = []
         answered = []
@@ -46,14 +68,21 @@ class User:
                 "Outcome": outcome}
 
         data = pd.DataFrame(data)
-        #data['Outcome'] = data['Response'] == data['Correct']
-
         return data
 
     def htmlTable(self):
-        return self.getPerformance().to_html()
+        """
+        Returns HTML rendering of joined performance table
+        """
+        return self.joinOnOriginal().to_html()
 
     def joinOnOriginal(self):
+        """
+        Joins performance data with starting data
+
+        returns dataframe of performance data as well
+        as the subject, school grade, and question
+        """
 
         perf = self.getPerformance()
         keep = list(perf.columns) + ['schoolGrade', 'question', 'subject']
@@ -62,11 +91,23 @@ class User:
         return joined
 
     def getSubject(self, category):
+        """
+        category is "Physics" or "Biology"
+
+        returns dataframe of those questions
+        that have been answered
+        """
         data = self.joinOnOriginal()
         subset = data[data['subject'] == category]
         return subset
 
     def subjectAccuracy(self, category):
+        """
+        category is "Physics" or "Biology"
+
+        returns [total answered, total correct, accuracy]
+        """
+
         data = self.getSubject(category)
 
         count = data.shape[0]
